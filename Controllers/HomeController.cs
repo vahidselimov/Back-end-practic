811bb973a8b0bb0d862e6980cbc0abdb85ef4cf5
vahidselimov <100331397+vahidselimov@users.noreply.github.com>
2022-05-23 12:38:22 +0400

    Practic

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Back_end_practic1.Controllers
+{
+    public class HomeController:Controller
+    {
+        //public ContentResult Index()
+        //{
+        //    return Content("Hello Ap204");
+        //}
+        //public JsonResult Index()
+        //{
+        //    return Json("hello");
+        //}
+        public ViewResult Index()
+        {
+            ViewResult view = new ViewResult();
+            view.ViewName="Index";
+            return view;
+        }
+        public ViewResult About()
+        {
+            ViewResult view = new ViewResult();
+            view.ViewName = "About";
+            return view;
+        }
+        public ActionResult Content(int id)
+        {
+
+            ViewBag.product = "Cola";
+            ViewData["count"] = 3;
+            TempData["type"] = "type";
+            return RedirectToAction("About");
+            return View();
+        }
+        public ActionResult Faq(string name,string surname)
+        {
+            string fullname = $"{name} {surname}";
+            return Content(fullname);
+        }
+    }
+}
